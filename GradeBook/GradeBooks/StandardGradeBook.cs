c6e179fb12876e4c1c7171dae1507bdc01a96cd9
Hubert Chylka <chylkahubert@gmail.com>
2026-03-04 20:36:07 +0100

    stworzenie klasy standardgradebook

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBooks.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name, bool isWeighted)
+            : base(name,  isWeighted) 
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
